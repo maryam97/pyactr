c9c8c1aff5ed3c1def1f3a29cff58da80d4cdc5a
Maryam Meghdadi <marmegh@gmail.com>
2025-02-20 14:13:18 +0100

    activation trace off

--- priming/spp_freq.py
+++ priming/spp_freq.py
@@ -190,7 +190,7 @@ def run_stimulus(target, prime):
     prime_model.model.model_parameters['buffer_spreading_activation'] = {"imaginal": 1}
     prime_model.model.model_parameters['spreading_activation_restricted'] = True
     prime_model.model.model_parameters['association_only_from_chunks'] = False
-    prime_model.model.model_parameters['activation_trace'] = True
+    # prime_model.model.model_parameters['activation_trace'] = True
 
 
 
